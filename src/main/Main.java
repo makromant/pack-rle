2edc7b5b0f1860f2b8104c2bf3998849f4e84aff
makromant <makromant@gmail.com>
2018-04-10 23:04:48 +0300

    Did the compression and decompression of the text file with out tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import packer.Packer;
+import unpacker.Unpacker;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("compiled");
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("pack-rle [-z|-u] input.txt [-out output.txt]");
+        String cmd;
+        String[] splitedCmd;
+        while (true) {
+            cmd = in.nextLine();
+            splitedCmd = cmd.split(" ");
+            if (!splitedCmd[0].equals("pack-rle") || splitedCmd.length < 3
+                    || (!splitedCmd[1].equals("-z") && !splitedCmd[1].equals("-u"))) {
+                System.out.println("Wrong input. Try again.");
+            } else
+                break;
+        }
+        String inputName, outputName = null;
+        inputName = "txt/" + splitedCmd[2];
+
+        if (splitedCmd.length == 5) {
+            outputName = "txt/" + splitedCmd[4];
+        }
+
+        if (splitedCmd[1].equals("-z")) {
+            if (outputName == null)
+                outputName = outputName = "txt/co_" + splitedCmd[2];
+            Packer.compress(inputName, outputName);
+        }
+        else if (splitedCmd[1].equals("-u")) {
+            if (outputName == null)
+                outputName = outputName = "txt/deco_" + splitedCmd[2];
+            Unpacker.decompress(inputName, outputName);
+        }
     }
 }
--- src/main/unpacker/Unpacker.java
+++ src/main/unpacker/Unpacker.java
@@ -1,5 +1,53 @@
 package unpacker;
 
-public class Unpacker {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
+public class Unpacker {
+    public static void decompress(String inputName, String outputName) {
+        try (FileReader reader = new FileReader(inputName)) {
+            FileWriter writer = new FileWriter(outputName);
+            Scanner readLine = new Scanner(reader);
+            while (readLine.hasNextLine()) {
+                writer.write(decompressLine(readLine.nextLine()));
+                if (readLine.hasNextLine())
+                    writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    private static short[] charToShort(char[] list) {
+        short[] result = new short[list.length];
+        for (int i = 0; i < list.length; i++) {
+            result[i] = (short) list[i];
+        }
+        return result;
+    }
+
+    private static String decompressLine(String line) {
+        if (line.length() <= 1) return line;
+        short[] inStr = charToShort(line.toCharArray());
+        String result = "";
+        int index = 0;
+        while (index != inStr.length) {
+            if (inStr[index] < 0) {
+                for (int i = 0; i < inStr[index] + Short.MAX_VALUE + 1; i++)
+                    result += (char)inStr[index + 1];
+                index += 2;
+            } else {
+                for (int i = 0; i < inStr[index]; i++) {
+                    result += (char)inStr[index + i + 1];
+                }
+                index += inStr[index] + 1;
+            }
+        }
+        return result;
+    }
 }
